74b1bc55693aba2574f0e6c7d5739612a346f8ae
Angel <gutierrezramosjoseangel@gmail.com>
2021-05-15 17:19:11 -0500

    Flat List Component usage

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { FlatList, View, Text } from 'react-native';
+
+interface MenuItem {
+    name: string;
+    icon: string;
+    component: string;
+}
+
+const menuItems = [
+    {
+        name: 'Animation 01',
+        icon: 'cube-outline',
+        component: 'Animation101'
+    }
+]
 
 export const Home = () => {
+    const renderMenuItem = (menuItem: MenuItem) => {
+        return (
+            <View>
+                <Text>{ menuItem.name } - { menuItem.icon }</Text>
+            </View>
+        )
+    }
+
     return (
-        <View>
-            <Text>Home</Text>
-            <Icon 
-                name="star-outline"
-                size= { 50 }
-                color="black"
+        <View style={{ flex: 1 }}>
+            <FlatList 
+                data={ menuItems }
+                renderItem={ ({ item }) => renderMenuItem(item) }
             />
         </View>
     )
