4ce3aaa0bc68b9ff20dde66e7962a08186eaa901
Angel <gutierrezramosjoseangel@gmail.com>
2021-05-16 19:08:59 -0500

    Reusable header

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { FlatList, View, Text } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FlatList, View } from 'react-native';
 import { FlatListMenuItem } from '../components/FlatListMenuItem';
 import { styles } from '../theme/appTheme';
 import { menuItems } from '../data/menuItems';
+import { HeaderTitle } from '../components/HeaderTitle';
 
 export const Home = () => {
-    const { top } = useSafeAreaInsets();
-
-    const renderListHeader = () => {
-        return (
-            <View style={{ marginTop: top + 20, marginBottom:20 }}>
-                <Text style={ styles.title }>Menu Options</Text>
-            </View>
-        )
-    }
-
     const itemSeparator = () => {
         return (
             <View 
@@ -34,7 +24,7 @@ export const Home = () => {
                 data={ menuItems }
                 renderItem={ ({ item }) => <FlatListMenuItem menuItem={ item } /> }
                 keyExtractor={ (item) => item.name }
-                ListHeaderComponent={ renderListHeader }
+                ListHeaderComponent={ () => <HeaderTitle title='Opciones de menú' /> }
                 ItemSeparatorComponent={ itemSeparator }
             />
         </View>
--- src/views/SwitchScreen.tsx
+++ src/views/SwitchScreen.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { View, Switch, Platform } from 'react-native';
+import { HeaderTitle } from '../components/HeaderTitle';
 
 export const SwitchScreeen = () => {
     const [isEnabled, setIsEnabled] = useState(false);
     const toggleSwitch = () => setIsEnabled(!isEnabled);
 
     return (
-        <View style={{ marginTop: 100 }}>
+        <View style={{ marginHorizontal: 20 }}>
+            <HeaderTitle title='Switches' />
             <Switch
                 trackColor={{ false: "#D9D9DB", true: "green" }}
                 thumbColor={(Platform.OS === 'android') ? 'white' : ''}
